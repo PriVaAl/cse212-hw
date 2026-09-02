c14636e6940ca900fc6803f1d5f934db6d5cee81
PriVaAl <priscilla.valverde@gmail.com>
2026-09-01 20:24:51 -0600

    W01 Code: Dynamic Arrays completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        //Step 1: I will create a fixed size array to hold the results, since I already know exactly how many 
+        //multiples I need using the parameter length 
+        double[] result = new double[length]; 
+        
+        //Step 2: From index 0 up to length (not includiing it), loop each position in an array. At each position i
+        //calculate the multiple by multipling the number by (i + 1), then store it in result[i]
+        for (int i = 0; i < length; i++ )
+        {
+            result[i] = number * (i + 1);
+        }
 
-        return []; // replace this return statement with your own
+        return result; //Step 3: Then return the complete array back 
     }
 
     /// <summary>
@@ -29,5 +40,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Step 1: Save the last 'amount' items (these will move to the front) to do so we need to a subtraction 
+        //to know how many places we are moving to the front
+        List<int> lastAmount = data.GetRange(data.Count - amount, amount);
+
+        //Step 2: Save everything else (that would be the first part of the list as it was originally)
+        List<int> firstAmount = data.GetRange(0, data.Count - amount);
+
+        //Step 3: Clear out that original list completely 
+        data.RemoveRange(0, data.Count);
+
+        //Step 4:Add the pieces back in the new order, lastAmount then firstAmount 
+        data.AddRange(lastAmount);
+        data.AddRange(firstAmount);
     }
 }
